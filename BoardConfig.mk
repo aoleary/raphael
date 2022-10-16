c0758b1ec0ef20d1e29992fa65732857aa4a2f4a
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-10-16 14:29:21 +0530

    raphael: set BUILD_BROKEN_MISSING_REQUIRED_MODULES to true
    TARGET module FaceUnlockService requires non-existent TARGET module: faceunlock_vendor_dependencies
    build/make/core/main.mk:767: warning: Set BUILD_BROKEN_MISSING_REQUIRED_MODULES := true to bypass this check if this is intentional

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,8 @@ BOARD_VENDOR := xiaomi
 
 DEVICE_PATH := device/xiaomi/raphael
 
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
